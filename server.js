3347202027383f4f3c1537751fe5944ee12da574
BscCohort Admin <bsc.cohort.io@gmail.com>
2024-01-27 07:28:52 +0000

    connect to database initiate

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const connectDB =  require("./config/db");
+const cors = require("cors");
+
+// CONNECINGTO MONGODB
+connectDB();
+
+
 
 // Initiating the app
 const app = express();
+
+// Enabling CORS here for safety of access of info from other domains
+app.use(cors());
+// Handle JSON parsing properly
+app.use(express.json())
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
